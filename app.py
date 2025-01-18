c713404c20073120827f9cef4a9676c76e0adf00
IYXYI <55164519+IYXYI@users.noreply.github.com>
2025-01-18 23:02:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return send_from_directory(".", "index.html")  # Serve index.html from the root directory
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
